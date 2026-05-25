0e7a00f4a8b16cbd4b2d96ab5eb5f0050dd7a7bc
Meetpadsala <2402031030051@silveroakuni.ac.in>
2026-05-25 12:12:08 +0530

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:2234/api",
+  baseURL: "http://192.168.29.13:2234/api",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
